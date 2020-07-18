6b02e03222ba25594b4c91107a92de04525c78ff
Leah Therese Totland <55258334+lherese@users.noreply.github.com>
2020-07-18 16:11:27 +0200

    Refines test for dice rolling.

--- Tests/DiceBagTests/DiceTests.swift
+++ Tests/DiceBagTests/DiceTests.swift
@@ -8,25 +8,13 @@ final class DiceTests: XCTestCase {
     let sides = 6
     let die = Die(d: sides)
 
-    var tooHigh = false
-    var tooLow = false
+    var rolls: Set<Int> = []
 
     for _ in 1...1000 {
-      let roll = die.roll()
-
-      if roll > sides {
-        tooHigh = true
-        break
-      }
-
-      if roll < 1 {
-        tooLow = true
-        break
-      }
+      rolls.insert(die.roll())
     }
 
-    XCTAssertFalse(tooHigh, "Dice rolled too high.")
-    XCTAssertFalse(tooLow, "Dice rolled too low.")
+    XCTAssertEqual(rolls, [1, 2, 3, 4, 5, 6], "Dice rolled invalid values.")
   }
 
 }
