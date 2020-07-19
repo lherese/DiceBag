b749034a803ddc34aba3730fd9ba6c934dea3650
Leah Therese Totland <55258334+lherese@users.noreply.github.com>
2020-07-19 13:23:23 +0200

    Implements multiplication and maluses.

--- Sources/DiceBag/Specification.swift
+++ Sources/DiceBag/Specification.swift
@@ -1,7 +1,10 @@
+import Foundation
+
 public struct Specification: Equatable {
-  enum Entry: Equatable {
+  indirect enum Entry: Equatable {
     case die(_ count: Int, die: Die)
     case constant(_ constant: Int)
+    case multiplier(_ multiplier: Entry, _ entry: Entry)
 
     func roll() -> Int {
       switch self {
@@ -15,30 +18,76 @@ public struct Specification: Equatable {
         return result
       case let .constant(constant):
         return constant
+      case let .multiplier(multiplier, entry):
+        return multiplier.roll() * entry.roll()
       }
+    }
+
+    init?(_ specification: Substring) {
+      self.init(String(specification))
     }
 
     init?(_ specification: String) {
       if let constant = Int(specification) {
         self = .constant(constant)
+      } else if specification.contains("*") {
+        let elements = specification
+          .split(separator: "*", maxSplits: 1)
+
+        guard
+          let entry = Entry(elements.first!),
+          let multiplier = Entry(elements.last!)
+        else {
+          return nil
+        }
+
+        self = .multiplier(entry, multiplier)
+      } else if specification.contains("d") {
+        let elements = specification
+          .split(separator: "d", maxSplits: 1, omittingEmptySubsequences: false)
+
+        guard
+          let count = (elements.first! == "" ? 1 : Int(elements.first!)),
+          let die = Int(elements.last!)
+        else {
+          return nil
+        }
+
+        self = .die(count, die: Die(d: die))
       } else {
-        self = .die(1, die: Die(d: 6))
+        return nil
       }
     }
   }
 
   let entries: [Entry]
 
+  public init?(_ specification: Substring) {
+    self.init(String(specification))
+  }
+
   public init?(_ specification: String) {
-    let trimmedSpecification = specification.filter { character in
-      !character.isWhitespace
+    let entries = specification
+      .filter { !$0.isWhitespace }
+      .replacingOccurrences(of: "-", with: "+-")
+      .split(separator: "+")
+      .map { Entry($0) }
+
+    guard
+      entries.allSatisfy({ $0 != nil })
+    else {
+      return nil
     }
 
-    self.entries = []
+    self.init(entries: entries.compactMap({ $0 }))
+  }
+
+  init(entries: [Entry]) {
+    self.entries = entries
   }
 
   init(_ entries: Entry...) {
-    self.entries = entries
+    self.init(entries: entries)
   }
 
   public func roll() -> Int {
--- Tests/DiceBagTests/DiceTests.swift
+++ Tests/DiceBagTests/DiceTests.swift
@@ -36,13 +36,15 @@ final class DiceTests: XCTestCase {
   }
 
   func testSpecification() {
-    guard
-      let specification = Specification("3d6 + 3")
-    else {
-      return XCTFail("Specification is nil.")
-    }
+    XCTAssertEqual(Specification("d6"), Specification(1 * Die(d: 6)))
+    XCTAssertEqual(Specification("3d6"), Specification(3 * Die(d: 6)))
+    XCTAssertEqual(Specification("3d6 + 3"), Specification(3 * Die(d: 6), +3))
+    XCTAssertEqual(Specification("3d6 - 2"), Specification(3 * Die(d: 6), +(-2)))
+    XCTAssertEqual(Specification("3d6 * 2"), Specification(.multiplier(3 * Die(d: 6), +2)))
+  }
 
-    XCTAssertEqual(specification, Specification(3 * Die(d: 6), +3))
+  func testInvalidSpecification() {
+    XCTAssertNil(Specification("2d3d2"), "Double dice marker in specification.")
   }
 
 }
